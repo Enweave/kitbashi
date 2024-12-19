130cd9fdf44e8dd0bf040f8514b687d88e851964
Enweave <enweave@gmail.com>
2024-12-19 22:34:59 +0500

    Keep player in bounds

--- src/engine/Actors/Player.ts
+++ src/engine/Actors/Player.ts
@@ -1,18 +1,46 @@
-import {Actor} from "./ActorsBase.ts";
+import {Actor, EntityType} from "./ActorsBase.ts";
 import {InputController} from "../InputController.ts";
 import {Vector2} from "../Utils.ts";
+import {ASPECT_RATIO, VIEWPORT_WIDTH} from "../Constants.ts";
 
 
 export class Player extends Actor {
     input: InputController;
+    entityType = EntityType.Player;
+
+    LEFT_BOUND = this.radius;
+    RIGHT_BOUND = VIEWPORT_WIDTH-this.radius;
+    TOP_BOUND = this.radius;
+    BOTTOM_BOUND = VIEWPORT_WIDTH/ASPECT_RATIO-this.radius;
 
     constructor(input: InputController) {
         super();
         this.input = input;
     }
 
+    keepInBounds() {
+        if(this._body.pos.x < this.LEFT_BOUND) {
+            this._body.pos.x = this.LEFT_BOUND;
+            this._currentVelocity.x = 0;
+        }
+        if(this._body.pos.x > this.RIGHT_BOUND) {
+            this._body.pos.x = this.RIGHT_BOUND;
+            this._currentVelocity.x = 0;
+        }
+        if(this._body.pos.y < this.TOP_BOUND) {
+            this._body.pos.y = this.TOP_BOUND;
+            this._currentVelocity.y = 0;
+        }
+        if(this._body.pos.y > this.BOTTOM_BOUND) {
+            this._body.pos.y = this.BOTTOM_BOUND;
+            this._currentVelocity.y = 0;
+        }
+    }
+
     tick(delta: number) {
         this.controllerDirection = this.input.getDirection() as Vector2;
+        this.keepInBounds();
         super.tick(delta);
+
     }
 }
--- src/engine/Constants.ts
+++ src/engine/Constants.ts
@@ -1,3 +1,3 @@
 export const ASPECT_RATIO = 10 / 9;
-export const VIEWPORT_WIDTH = 500;
+export const VIEWPORT_WIDTH = 320 ;
 export const LOW_VELOCITY_THRESHOLD = 0.01;
