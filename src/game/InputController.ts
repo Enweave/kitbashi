8fc6fef1b807d63bacee134d72f224fe0ff37201
Insy Weaver <enweave@gmail.com>
2024-12-28 04:52:54 +0500

    Input - switch to codes

--- src/game/InputController.ts
+++ src/game/InputController.ts
@@ -17,18 +17,18 @@ interface MovementDirection {
 }
 
 export class InputController {
-  keyState: Ref<Map<InputActions, boolean>> = ref(new Map());
+  actionState: Ref<Map<InputActions, boolean>> = ref(new Map());
   private flowController: FlowController;
-  private keyMap: Map<string, InputActions> = new Map();
+  private keyCodeMap: Map<string, InputActions> = new Map();
 
   constructor(inFlowController: FlowController) {
-    this.keyMap.set('a', InputActions.left);
-    this.keyMap.set('d', InputActions.right);
-    this.keyMap.set('w', InputActions.up);
-    this.keyMap.set('s', InputActions.down);
-    this.keyMap.set('j', InputActions.fire);
-    this.keyMap.set('k', InputActions.special);
-    this.keyMap.set('Escape', InputActions.pause);
+    this.keyCodeMap.set('KeyA', InputActions.left);
+    this.keyCodeMap.set('KeyD', InputActions.right);
+    this.keyCodeMap.set('KeyW', InputActions.up);
+    this.keyCodeMap.set('KeyS', InputActions.down);
+    this.keyCodeMap.set('KeyJ', InputActions.fire);
+    this.keyCodeMap.set('KeyK', InputActions.special);
+    this.keyCodeMap.set('Escape', InputActions.pause);
 
     this.flowController = inFlowController;
 
@@ -36,9 +36,9 @@ export class InputController {
       if (e.repeat) {
         return;
       }
-      const action = this.keyMap.get(e.key);
+      const action = this.keyCodeMap.get(e.code);
       if (action) {
-        this.keyState.value.set(action, true);
+        this.actionState.value.set(action, true);
         if (action === InputActions.pause) {
           this.flowController.togglePause();
         }
@@ -46,9 +46,9 @@ export class InputController {
     });
 
     window.addEventListener('keyup', (e) => {
-      const action = this.keyMap.get(e.key);
+      const action = this.keyCodeMap.get(e.code);
       if (action) {
-        this.keyState.value.set(action, false);
+        this.actionState.value.set(action, false);
       }
     });
   }
@@ -56,19 +56,19 @@ export class InputController {
   getDirection(): MovementDirection {
     const direction: MovementDirection = { x: 0, y: 0 };
 
-    direction.x += this.keyState.value.get(InputActions.left) ? -1 : 0;
-    direction.x += this.keyState.value.get(InputActions.right) ? 1 : 0;
-    direction.y += this.keyState.value.get(InputActions.up) ? -1 : 0;
-    direction.y += this.keyState.value.get(InputActions.down) ? 1 : 0;
+    direction.x += this.actionState.value.get(InputActions.left) ? -1 : 0;
+    direction.x += this.actionState.value.get(InputActions.right) ? 1 : 0;
+    direction.y += this.actionState.value.get(InputActions.up) ? -1 : 0;
+    direction.y += this.actionState.value.get(InputActions.down) ? 1 : 0;
 
     return direction;
   }
 
   isFiring(): boolean {
-    return this.keyState.value.get(InputActions.fire) || false;
+    return this.actionState.value.get(InputActions.fire) || false;
   }
 
   isSpecial(): boolean {
-    return this.keyState.value.get(InputActions.special) || false;
+    return this.actionState.value.get(InputActions.special) || false;
   }
 }
